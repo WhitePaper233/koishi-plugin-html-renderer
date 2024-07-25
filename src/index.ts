578333defa6ed784dc565331994805d46ffbed0b
WhitePaper233 <1423634265@qq.com>
2024-07-25 16:19:45 +0800

    update: Update package.json version to 1.0.0 and koishi dependency to 4.17.9, changed renderer api, cancel auto browser download, fix memory leak bug

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,24 @@
 import { Context, Schema } from "koishi";
-import { HTMLRenderer, RendererOptions } from "./renderer";
+import HTMLRenderer, { BrowserOptions, AVALIABLE_BROWSERS } from "./renderer";
 
 declare module "koishi" {
-  interface Context {
-    html_renderer: HTMLRenderer;
-  }
+    interface Context {
+        html_renderer: HTMLRenderer;
+    }
 }
 
-export const Config: Schema<RendererOptions> = Schema.object({
-  renderer_browser: Schema.union(["chromium", "webkit", "firefox"])
-    .default("chromium")
-    .description("默认使用的浏览器"),
-  browser_channel: Schema.string().description("浏览器通道"),
-  browser_download_host: Schema.string().description("浏览器下载镜像"),
-  browser_proxy_host: Schema.string().description("浏览器代理地址"),
+export const Config: Schema<BrowserOptions> = Schema.object({
+    renderer_browser: Schema.union(AVALIABLE_BROWSERS)
+        .default("chromium")
+        .description("默认使用的浏览器"),
+    browser_channel: Schema.string().description("浏览器通道"),
+    browser_proxy_host: Schema.string().description("浏览器代理地址"),
 }).description("HTML 浏览器配置");
 
 export const name = "html_renderer";
 
-export function apply(ctx: Context, config: RendererOptions) {
-  ctx.plugin(HTMLRenderer, config);
+export function apply(ctx: Context, config: BrowserOptions) {
+    ctx.plugin(HTMLRenderer, config);
 }
 
 // HTML 渲染器类
--- src/renderer.ts
+++ src/renderer.ts
@@ -3,228 +3,383 @@ import { readFile } from "fs/promises";
 import { existsSync } from 'fs'
 import { Browser, LaunchOptions, chromium, firefox, webkit } from "playwright";
 import { Context, Service } from "koishi";
-import { exec } from "child_process";
-import { promisify } from "util";
 
-interface RendererOptions {
-  /**
-   * 浏览器类型
-   * @default chromium
-   * @see https://playwright.dev/docs/next/api/class-browsertype
-   */
-  renderer_browser?: string;
+export const AVALIABLE_BROWSERS = ["firefox", "webkit", "chromium"] as const;
+type BrowserType = typeof AVALIABLE_BROWSERS[number];
 
-  /**
-   * 浏览器下载地址
-   */
-  browser_download_host?: string;
+// 浏览器选项
+export interface BrowserOptions {
+    /**
+     * 浏览器类型
+     * @default chromium
+     * @see https://playwright.dev/docs/next/api/class-browsertype
+     */
+    renderer_browser?: BrowserType;
 
-  /**
-   * 浏览器代理地址
-   */
-  browser_proxy_host?: string;
+    /**
+     * 浏览器代理地址
+     */
+    browser_proxy_host?: string;
 
-  /**
-   * 浏览器通道
-   */
-  browser_channel?: string;
+    /**
+     * 浏览器通道
+     */
+    browser_channel?: string;
+}
+
+// 页面参数
+interface PageOptions {
+    // 视口
+    viewport: { width: number; height: number };
+    // 基础 URL
+    base_url: string;
+}
+
+// 渲染选项
+interface RenderOptions {
+    // 等待时间
+    wait_time: number;
+    // 类型
+    type: "png" | "jpeg";
+    // 质量
+    quality: number;
+    // 缩放
+    scale: number;
 }
 
 // HTML 渲染器服务类
-class HTMLRenderer extends Service {
-  // 浏览器对象
-  protected browser: Browser;
+export default class HTMLRenderer extends Service {
+    // 浏览器对象
+    protected browser: Browser;
 
-  // 选项
-  protected options: RendererOptions = {
-    renderer_browser: "",
-    browser_download_host: "",
-    browser_proxy_host: "",
-    browser_channel: "",
-  };
+    // 选项
+    protected options: BrowserOptions = {
+        renderer_browser: "chromium",
+        browser_proxy_host: "",
+        browser_channel: "",
+    };
 
-  // 构造方法
-  public constructor(ctx: Context, renderer_options?: RendererOptions) {
-    super(ctx, "html_renderer", true);
     // 初始化日志
-    this.logger = this.ctx.logger("html_renderer");
+    private _logger = this.ctx.logger("html_renderer");
 
-    // 初始化配置
-    this.options = Object.assign(this.options, renderer_options);
-  }
-
-  // 声明周期函数
-  // 启动
-  public async start() {
-    // 检查浏览器是否安装
-    let browser_exsit: boolean = false;
-    switch (this.options.renderer_browser) {
-      case 'firefox': {
-        browser_exsit = existsSync(firefox.executablePath());
-        break;
-      }
-      case 'webkit': {
-        browser_exsit = existsSync(webkit.executablePath());
-        break;
-      }
-      default: {
-        browser_exsit = existsSync(chromium.executablePath());
-        break;
-      }
+    // 构造方法
+    public constructor(ctx: Context, renderer_options?: BrowserOptions) {
+        super(ctx, "html_renderer", true);
+        // 初始化配置
+        this.options = Object.assign(this.options, renderer_options);
     }
 
-    if (!browser_exsit) {
-      this.logger.info("未检测到浏览器，即将安装浏览器");
-      await this.install_browser();
+    // 声明周期函数
+    // 启动
+    public async start() {
+        // 检查浏览器是否安装
+        let browser_exsit: boolean = false;
+
+        switch (this.options.renderer_browser) {
+            case 'firefox': {
+                browser_exsit = existsSync(firefox.executablePath());
+                break;
+            }
+            case 'webkit': {
+                browser_exsit = existsSync(webkit.executablePath());
+                break;
+            }
+            default: {
+                browser_exsit = existsSync(chromium.executablePath());
+                break;
+            }
+        }
+
+        if (!browser_exsit) {
+            this._logger.error("未检测到浏览器，访问 https://playwright.dev/docs/browsers 以安装浏览器");
+            throw new Error("未检测到浏览器");
+        }
+
+        // 配置启动参数
+        const launch_options: LaunchOptions = {};
+        if (this.options.browser_proxy_host) {
+            launch_options.proxy.server = this.options.browser_proxy_host;
+        }
+        if (this.options.browser_channel) {
+            launch_options.channel = this.options.browser_channel;
+        }
+
+        // 启动浏览器
+        this._logger.debug("正在启动浏览器...");
+        switch (this.options.renderer_browser) {
+            case 'firefox': {
+                this._logger.debug("正在以 firefox 浏览器启动服务");
+                this.browser = await firefox.launch(launch_options);
+                break;
+            }
+            case 'webkit': {
+                this._logger.debug("正在以 webkit 浏览器启动服务");
+                this.browser = await webkit.launch(launch_options);
+                break;
+            }
+            default: {
+                this._logger.debug("正在以 chromium 浏览器启动服务");
+                this.browser = await chromium.launch(launch_options);
+                break;
+            }
+        }
     }
 
-    // 配置启动参数
-    const launch_options: LaunchOptions = {};
-    if (this.options.browser_proxy_host) {
-      launch_options.proxy.server = this.options.browser_proxy_host;
-    }
-    if (this.options.browser_channel) {
-      launch_options.channel = this.options.browser_channel;
+    // 停止
+    public async stop() {
+        // 关闭浏览器
+        this._logger.debug("正在关闭浏览器...");
+        await this.browser?.close();
     }
 
-    // 启动浏览器
-    this.logger.debug("正在启动浏览器...");
-    if (this.options.renderer_browser === "firefox") {
-      this.logger.debug("正在以 firefox 浏览器启动服务");
-      this.browser = await firefox.launch(launch_options);
-    } else if (this.options.renderer_browser === "webkit") {
-      this.logger.debug("正在以 webkit 浏览器启动服务");
-      this.browser = await webkit.launch(launch_options);
-    } else {
-      this.logger.debug("正在以 chromium 浏览器启动服务");
-      this.browser = await chromium.launch(launch_options);
-    }
-  }
+    // 模板渲染方法
+    // 渲染HTML
+    /**
+     * 渲染 HTML 模板到图片
+     *
+     * @param template_path - HTML 模板路径
+     * @param template_name - HTML 模板文件名
+     * @param templates_args - HTML 模板参数
+     * @param page_options - 页面参数 (optional, default: { 'viewport': { 'width': 800, 'height': 600 }, 'base_url': `file://${process.cwd()}/templates/` }).
+     * @param render_options - 渲染选项 (optional, default: { { wait_time: 0, type: "png", quality: undefined, scale: 2 } })
+     * @returns - 返回图片的 Buffer
+     */
+    public async render_template_html_file(
+        template_path: string,
+        template_name: string,
+        templates_arg: Object,
+        page_options: PageOptions = {
+            viewport: { width: 800, height: 600 },
+            base_url: `file://${template_path}/`,
+        },
+        render_options: RenderOptions = {
+            wait_time: 0,
+            type: "png",
+            quality: undefined,
+            scale: 2,
+        }
+    ): Promise<Buffer> {
+        // 读取模板文件
+        let template: string;
+        try {
+            const data = await readFile(`${template_path}/${template_name}`, "utf-8");
+            template = data;
+        } catch (error) {
+            this._logger.error(`读取模板文件失败: ${error.message}`);
+            throw error;
+        }
 
-  // 停止
-  public async stop() {
-    // 关闭浏览器
-    this.logger.debug("正在关闭浏览器...");
-    await this.browser?.close();
-  }
+        // 渲染模板
+        const html = await render(template, templates_arg, { async: true });
 
-  // 渲染方法
-  // 渲染HTML
-  /**
-   * 渲染 HTML 模板到图片
-   *
-   * @param template_path - HTML 模板路径
-   * @param template_name - HTML 模板文件名
-   * @param templates_args - HTML 模板参数
-   * @param page_args - 页面参数 (optional, default: { 'viewport': { 'width': 800, 'height': 600 }, 'base_url': `file://${process.cwd()}/templates/` }).
-   * @param render_options - 渲染选项 (optional, default: { { wait_time: 0, type: "png", quality: undefined, scale: 2 } })
-   * @returns - 返回图片的 Buffer
-   */
-  public async render_html(
-    template_path: string,
-    template_name: string,
-    templates_arg: Object,
-    page_args = {
-      viewport: { width: 800, height: 600 },
-      base_url: `file://${template_path}/`,
-    },
-    render_options: {
-      wait_time: number,
-      type: "png" | "jpeg",
-      quality: number,
-      scale: number,
-    } = {
-      wait_time: 0,
-      type: "png",
-      quality: undefined,
-      scale: 2,
-    }
-  ): Promise<Buffer> {
-    // 读取模板文件
-    let template: string;
-    await readFile(`${template_path}/${template_name}`, "utf-8")
-      .then((data) => {
-        template = data;
-      })
-      .catch((error) => {
-        this.logger.error(`读取模板文件失败: ${error.message}`);
-        throw error;
-      });
+        // 加载HTML页面
+        const page = await this.browser.newPage({
+            ...page_options,
+            deviceScaleFactor: render_options.scale,
+        });
+        await page.goto(template_path);
+        await page.setContent(html, { waitUntil: "networkidle" });
+        await page.waitForTimeout(render_options.wait_time);
 
-    // 渲染模板
-    const html = await render(template, templates_arg, { async: true });
+        // 截图
+        const image = await page.screenshot({
+            fullPage: true,
+            type: render_options.type,
+            quality: render_options.quality,
+        });
 
-    // 加载HTML页面
-    const page = await this.browser.newPage({
-      viewport: page_args.viewport,
-      baseURL: page_args.base_url,
-      deviceScaleFactor: render_options.scale,
-    });
-    await page.goto(template_path);
-    await page.setContent(html, { waitUntil: "networkidle" });
-    await page.waitForTimeout(render_options.wait_time);
+        // 关闭页面
+        page.close();
 
-    // 截图
-    const image = await page.screenshot({
-      fullPage: true,
-      type: render_options.type,
-      quality: render_options.quality,
-    });
-
-    return image;
-  }
-
-  // 安装浏览器
-  protected async install_browser() {
-    // 如果设置了镜像源 则使用镜像源下载 否则使用默认源下载
-    if (this.options.browser_download_host) {
-      // 设置环境变量
-      process.env.PLAYWRIGHT_DOWNLOAD_HOST = this.options.browser_download_host;
-    } else {
-      // 设置环境变量
-      process.env.PLAYWRIGHT_DOWNLOAD_HOST =
-        "https://npmmirror.com/mirrors/playwright/";
+        return image;
     }
 
-    // 如果设置了代理 则使用代理下载
-    if (this.options.browser_proxy_host) {
-      // 设置环境变量
-      process.env.HTTP_PROXY = this.options.browser_proxy_host;
-      process.env.HTTPS_PROXY = this.options.browser_proxy_host;
+    // 渲染 HTML
+    /**
+     * 渲染 HTML 文件到图片
+     *
+     * @param file_path - HTML 文件路径
+     * @param file_name - HTML 文件名
+     * @param page_options - 页面参数 (optional, default: { 'viewport': { 'width': 800, 'height': 600 }, 'base_url': `file://${file_path}/` }).
+     * @param render_options - 渲染选项 (optional, default: { { wait_time: 0, type: "png", quality: undefined, scale: 2 } })
+     * @returns - 返回图片的 Buffer
+     */
+    public async render_html_file(
+        file_path: string,
+        file_name: string,
+        page_options: PageOptions = {
+            viewport: { width: 800, height: 600 },
+            base_url: `file://${file_path}/`,
+        },
+        render_options: RenderOptions = {
+            wait_time: 0,
+            type: "png",
+            quality: undefined,
+            scale: 2,
+        }
+    ): Promise<Buffer> {
+        // 读取 html 文件
+        let page_string = "";
+        try {
+            page_string = await readFile(`${file_path}/${file_name}`, "utf-8");
+        } catch (error) {
+            this._logger.error(`读取 html 文件失败: ${error.message}`);
+            throw error;
+        }
+
+        // 加载 HTML 页面
+        const page = await this.browser.newPage({
+            ...page_options,
+            deviceScaleFactor: render_options.scale,
+        });
+        await page.goto(file_path);
+        await page.setContent(page_string, { waitUntil: "networkidle" });
+        await page.waitForTimeout(render_options.wait_time);
+
+        // 截图
+        const image = await page.screenshot({
+            fullPage: true,
+            type: render_options.type,
+            quality: render_options.quality,
+        });
+
+        // 关闭页面
+        page.close();
+
+        return image;
     }
 
-    let install_command: string = "npx playwright install";
+    // 渲染 URL
+    /**
+     * 渲染 URL 到图片
+     *
+     * @param url - URL 地址
+     * @param page_options - 页面参数 (optional, default: { 'viewport': { 'width': 800, 'height': 600 }, 'base_url': url }).
+     * @param render_options - 渲染选项 (optional, default: { { wait_time: 0, type: "png", quality: undefined, scale: 2 } })
+     * @returns - 返回图片的 Buffer
+     */
+    public async render_url(
+        url: string,
+        page_options: PageOptions = {
+            viewport: { width: 800, height: 600 },
+            base_url: url,
+        },
+        render_options: RenderOptions = {
+            wait_time: 0,
+            type: "png",
+            quality: undefined,
+            scale: 2,
+        }
+    ): Promise<Buffer> {
+        // 加载 URL 页面
+        const page = await this.browser.newPage({
+            ...page_options,
+            deviceScaleFactor: render_options.scale,
+        });
+        await page.goto(url);
+        await page.waitForTimeout(render_options.wait_time);
 
-    // 如果设置了火狐浏览器 则安装火狐浏览器 否则安装chromium
-    let browser_type: string;
-    switch (this.options.renderer_browser) {
-      case 'firefox': {
-        browser_type = 'firefox';
-        break;
-      }
-      case 'webkit': {
-        browser_type = 'webkit';
-        break;
-      }
-      default: {
-        browser_type = 'chromium';
-        break;
-      }
+        // 截图
+        const image = await page.screenshot({
+            fullPage: true,
+            type: render_options.type,
+            quality: render_options.quality,
+        });
+
+        // 关闭页面
+        page.close();
+
+        return image;
     }
-    install_command += ` ${browser_type}`;
 
-    // 执行安装命令
-    this.logger.info(`正在安装浏览器: ${browser_type}`);
-    try {
-      await promisify(exec)(install_command);
-    } catch (error) {
-      this.logger.error(`安装浏览器失败: ${error.message}`);
-      throw error;
+    // 渲染文本 HTML 模板
+    /**
+     * 渲染文本 HTML 模板到图片
+     *
+     * @param template - HTML 模板
+     * @param templates_arg - HTML 模板参数
+     * @param page_options - 页面参数 (optional, default: { 'viewport': { 'width': 800, 'height': 600 }, 'base_url': `` }).
+     * @param render_options - 渲染选项 (optional, default: { { wait_time: 0, type: "png", quality: undefined, scale: 2 } })
+     * @returns - 返回图片的 Buffer
+     */
+    public async render_template_html(
+        template: string,
+        templates_arg: Object,
+        page_options: PageOptions = {
+            viewport: { width: 800, height: 600 },
+            base_url: ``,
+        },
+        render_options: RenderOptions = {
+            wait_time: 0,
+            type: "png",
+            quality: undefined,
+            scale: 2,
+        }
+    ): Promise<Buffer> {
+        // 渲染模板
+        const html = await render(template, templates_arg, { async: true });
+
+        // 加载 HTML 页面
+        const page = await this.browser.newPage({
+            ...page_options,
+            deviceScaleFactor: render_options.scale,
+        });
+        await page.setContent(html, { waitUntil: "networkidle" });
+        await page.waitForTimeout(render_options.wait_time);
+
+        // 截图
+        const image = await page.screenshot({
+            fullPage: true,
+            type: render_options.type,
+            quality: render_options.quality,
+        });
+
+        // 关闭页面
+        page.close();
+
+        return image;
     }
 
-    this.logger.info(`浏览器: ${browser_type} 安装成功`);
-  }
+    // 渲染文本 HTML
+    /**
+     * 渲染文本 HTML 到图片
+     *
+     * @param html - HTML 文本
+     * @param page_options - 页面参数 (optional, default: { 'viewport': { 'width': 800, 'height': 600 }, 'base_url': `` }).
+     * @param render_options - 渲染选项 (optional, default: { { wait_time: 0, type: "png", quality: undefined, scale: 2 } })
+     * @returns - 返回图片的 Buffer
+     */
+    public async render_html(
+        html: string,
+        page_options: PageOptions = {
+            viewport: { width: 800, height: 600 },
+            base_url: ``,
+        },
+        render_options: RenderOptions = {
+            wait_time: 0,
+            type: "png",
+            quality: undefined,
+            scale: 2,
+        }
+    ): Promise<Buffer> {
+        // 加载 HTML 页面
+        const page = await this.browser.newPage({
+            ...page_options,
+            deviceScaleFactor: render_options.scale,
+        });
+        await page.setContent(html, { waitUntil: "networkidle" });
+        await page.waitForTimeout(render_options.wait_time);
+
+        // 截图
+        const image = await page.screenshot({
+            fullPage: true,
+            type: render_options.type,
+            quality: render_options.quality,
+        });
+
+        // 关闭页面
+        page.close();
+
+        return image;
+    }
 }
-
-// 导出
-export { HTMLRenderer, RendererOptions };
